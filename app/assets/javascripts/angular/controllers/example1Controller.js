0569a46294ac77ea041d41929fde772ff6f467a3
Brian Butz <bbutz@pivotal.io>
2014-08-19 16:30:03 -0700

    Send and receive a ping

--- app/assets/javascripts/angular/controllers/example1Controller.js
+++ app/assets/javascripts/angular/controllers/example1Controller.js
@@ -1,19 +1,51 @@
 angular.module("XMPPLearnings").controller("example1Controller", function($scope, $timeout) {
+  var connection = null;
+
+  function clearLogs() {
+    $scope.logEntries = [];
+  }
+
+  function log(message) {
+    $scope.logEntries.push(message);
+  }
+
+  function receivePing() {
+    log('Received ping.');
+    connection.disconnect();
+    return false; // Strophe wants you to return false on callbacks that should only run once
+  }
+
+  function setupHandlers() {
+    log('Setting up handlers.');
+    connection.addHandler($timeout(receivePing), null, 'iq', null, 'ping1');
+  }
+
+  function sendPing() {
+    log('Sending ping.');
+
+    var domain = Strophe.getDomainFromJid(connection.jid);
+    var ping = (
+        new Strophe.Builder('iq', {to: domain, type: "get", id: "ping1"})
+        ).c("ping", {xmlns: "urn:xmpp:ping"});
+
+    connection.send(ping);
+  }
+
   function connected() {
-    $scope.logEntries.push('Connection established.')
+    log('Connection established.');
+    setupHandlers();
+    sendPing();
   }
 
   function disconnected() {
-    $scope.logEntries.push('Connection terminated.')
+    log('Connection terminated.');
   }
 
-  $scope.logEntries = [];
-
   this.connect = function(jid, password) {
-    $scope.logEntries.push('Starting to connect.')
-    var connection = new Strophe.Connection("http://192.168.123.45:5280/http-bind");
+    clearLogs();
+    log('Starting to connect.');
+    connection = new Strophe.Connection("http://192.168.123.45:5280/http-bind");
     connection.connect(jid, password, function(status) {
-      console.log(status);
       if (status === Strophe.Status.CONNECTED) {
         $timeout(connected);
       } else if (status === Strophe.Status.DISCONNECTED) {
