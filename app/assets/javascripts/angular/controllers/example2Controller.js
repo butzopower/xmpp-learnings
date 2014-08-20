4cd9c5359dbeb6fadcdd726260b9185de5a90832
Brian Butz <bbutz@pivotal.io>
2014-08-20 11:46:18 -0700

    Clean up

--- app/assets/javascripts/angular/controllers/example2Controller.js
+++ app/assets/javascripts/angular/controllers/example2Controller.js
@@ -6,12 +6,6 @@ angular.module("XMPPLearnings").controller("example2Controller", function($scope
     $scope.consoleEntries.push({html: html, type: type});
   }
 
-  function xml2html (xmlString) {
-    return xmlString.replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;");
-  }
-
   function showTraffic(body, type) {
     if (body.childNodes.length > 0 ) {
       angular.forEach(body.childNodes, function(node) {
@@ -24,8 +18,13 @@ angular.module("XMPPLearnings").controller("example2Controller", function($scope
   this.connect = function(jid, password) {
     $scope.connection = new Strophe.Connection("http://192.168.123.45:5280/http-bind");
 
-    $scope.connection.xmlInput = function(body) { $timeout(function() {showTraffic(body, 'incoming')});};
-    $scope.connection.xmlOutput = function(body) { $timeout(function() {showTraffic(body, 'outgoing')});};
+    $scope.connection.xmlInput = function(body) {
+      $timeout(function() { showTraffic(body, 'incoming') });
+    };
+
+    $scope.connection.xmlOutput = function(body) {
+      $timeout(function() { showTraffic(body, 'outgoing') });
+    };
 
     $scope.connection.connect(jid, password, function(status) {
       if (status === Strophe.Status.CONNECTED) {
