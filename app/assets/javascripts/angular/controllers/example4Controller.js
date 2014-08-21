75904f8bc6c86243845c0cc3309f6a15a2266793
Brian Butz <bbutz@pivotal.io>
2014-08-21 11:59:02 -0700

    Show presence of contacts

--- app/assets/javascripts/angular/controllers/example4Controller.js
+++ app/assets/javascripts/angular/controllers/example4Controller.js
@@ -1,28 +1,68 @@
 angular.module("XMPPLearnings").controller("example4Controller", function($scope, $timeout) {
   var connection = null;
   $scope.contacts = {};
+  $scope.connected = false;
+
+  // for testing
   $scope.jid = "foo@localhost";
   $scope.password = "bar";
 
-  function rosterReceived(iq) {
+  function getBareJid(jid) {
+    return Strophe.getBareJidFromJid(jid);
+  }
+
+  function getStatusFromPresence(presence) {
+    var presenceType = $(presence).attr('type');
+    if (presenceType == 'unavailable') return 'offline';
+
+    var show = $(presence).find('show').text();
+    if (show === "" || show === "chat") return 'online';
+
+    return 'away';
+  }
+
+  function presenceUpdated(presence) {
+    $timeout(function() {
+      var presenceType = $(presence).attr('type');
+      var from = $(presence).attr('from');
+      var jid = getBareJid(from);
+
+      if (presenceType !== 'error') {
+        var contact = $scope.contacts[jid];
+        if (contact) {
+          contact.status = getStatusFromPresence(presence);
+        }
+      }
+    });
+
+    return true;
+  }
+
+  function initialRosterReceived(iq) {
     $timeout(function() {
       $(iq).find('item').each(function() {
-        var jid = $(this).attr('jid');
+        var jid = getBareJid($(this).attr('jid'));
         var name = $(this).attr('name') || jid;
-        var contact = {name: name, jid: jid};
+        var contact = {name: name, jid: jid, status: 'offline'};
 
         $scope.contacts[jid] = contact;
       })
     });
+
+    connection.addHandler(presenceUpdated, null, "presence");
+    connection.send($pres());
   }
 
   function connected() {
+    $scope.connected = true;
     var iq = $iq({type: 'get'}).c('query', {xmlns: 'jabber:iq:roster'});
-    connection.sendIQ(iq, rosterReceived);
+    connection.sendIQ(iq, initialRosterReceived);
   }
 
   function disconnected() {
-
+    connection = null;
+    $scope.connected = false;
+    $scope.contacts = {};
   }
 
   this.connect = function(jid, password) {
@@ -35,5 +75,9 @@ angular.module("XMPPLearnings").controller("example4Controller", function($scope
         $timeout(disconnected);
       }
     });
-  }
+  };
+
+  this.disconnect = function() {
+    connection.disconnect();
+  };
 });
